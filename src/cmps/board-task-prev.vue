495e9077be9defc88154e5d7c0bdebad438338b3
InonBer <inon551@gmail.com>
2022-07-19 21:41:23 +0300

    work on frontend

---
+++ src/cmps/board-task-prev.vue
@@ -0,0 +1,26 @@
+<template>
+    <section class="task-prev-container">
+        <div class="task-prev">{{ task.title }}</div>
+
+    </section>
+</template>
+ <script>
+export default {
+    props: {
+        task: {
+            type: Object
+        }
+    },
+    name: 'TaskPrev',
+    components: {},
+    data() {
+        return {};
+    },
+    created() { },
+    methods: {},
+    computed: {},
+    unmounted() { },
+};
+</script>
+ <style>
+ </style>
