2fe1f67f40d8f1d31321405c481c045ae4e57b04
MaozYosifon <maozyosifon@gmail.com>
2022-07-20 11:17:18 +0300

    update

--- src/cmps/app-header.vue
+++ src/cmps/app-header.vue
@@ -2,12 +2,17 @@
   <header class="app-header">
     <div class="header-content">
       <h2 class="header-logo">Camallo</h2>
-      <button class="header-button">Workspaces<img src="../assets/arrow-down.png" alt="arw-dwn"/></button>
-      <button class="header-button">Recent <img src="../assets/arrow-down.png" alt="arw-dwn"/></button>
-      <button class="header-button">Starred<img src="../assets/arrow-down.png" alt="arw-dwn"/></button>
-      <button class="header-button">Templates<img src="../assets/arrow-down.png" alt="arw-dwn"/></button>
+      <button class="header-button">Workspaces<img src="../assets/arrow-down.png" alt="arw-dwn" /></button>
+      <button class="header-button">Recent <img src="../assets/arrow-down.png" alt="arw-dwn" /></button>
+      <button class="header-button">Starred<img src="../assets/arrow-down.png" alt="arw-dwn" /></button>
+      <button class="header-button">Templates<img src="../assets/arrow-down.png" alt="arw-dwn" /></button>
       <button class="header-create-button">Create</button>
       <button @click="$router.push('/board')" class="header-create-button">TEST BOARD</button>
+
+      <select id="boards" name="board-list">
+        <option v-for="board in bards" value=""></option>
+      </select>
+
     </div>
     <div class="header-end">
 
