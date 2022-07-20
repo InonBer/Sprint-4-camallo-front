1171eca4d88624c10aaa6036232e68dbc5c9ae0f
InonBer <inon551@gmail.com>
2022-07-20 12:06:43 +0300

    sync files

--- src/cmps/app-header.vue
+++ src/cmps/app-header.vue
@@ -7,7 +7,7 @@
       <button class="header-button">Starred<img src="../assets/arrow-down.png" alt="arw-dwn" /></button>
       <button class="header-button">Templates<img src="../assets/arrow-down.png" alt="arw-dwn" /></button>
       <button class="header-create-button">Create</button>
-      <button @click="$router.push('/board/')" class="header-create-button">TEST BOARD</button>
+      <button @click="$router.push('/board/b101')" class="header-create-button">TEST BOARD</button>
 
       <select v-if="boards" id="boards" name="board-list">
         <option>{{ boards[0].title }}</option>
