d5ffe9a520b885ce065b07d06d64ce9bf2185062
InonBer <inon551@gmail.com>
2022-07-24 23:44:10 +0300

    trying to fix dnd

--- src/cmps/group-prev.vue
+++ src/cmps/group-prev.vue
@@ -5,11 +5,11 @@
         type="text" :placeholder="group.title" v-model="groupTitle">
     </form>
     <header @click="isEdited = !isEdited" v-if="!isEdited" class="card-header"><span>{{
-        groupTitle
+        group.id
     }}</span>
     </header>
     <div class="group-card-scroll">
-      <task-list @enterClicked="onAddTask" @onTaskMode="onTaskMode" @saveTask="saveTask" @taskAdded="onBoardChange"
+      <task-list @enterClicked="onAddTask" @onTaskMove="onTaskMove" @saveTask="saveTask" @taskAdded="onBoardChange"
         @onBoardChange="onBoardChange" @onDetails="onDetails" :tasks="group.tasks" :groupId="group.id" />
     </div>
     <div v-if="!isTaskCreating" @click="onTaskAdding" class="add-btn"><span class="icon-plus"></span> Add a
@@ -33,7 +33,7 @@
 import taskList from './task-list.vue';
 import { boardService } from '../services/board.service';
 export default {
-  emits: ['onBoardChange', 'onTaskMode'],
+  emits: ['onBoardChange', 'onTaskMove'],
   props: {
     group: {
       type: Object
@@ -74,10 +74,9 @@ export default {
       group.tasks[idx] = task
       this.$store.dispatch('addTask', { group, id: group.id })
     },
-    onTaskMode(tasks) {
-
+    onTaskMove(tasks) {
       // const copy = JSON.parse(JSON.stringify(tasks))
-      this.$emit('onTaskMode', tasks, this.group.id)
+      this.$emit('onTaskMove', tasks, this.group.id)
       // this.items.tasks = JSON.parse(JSON.stringify(tasks))
       // let groupCopy = JSON.parse(JSON.stringify(this.group))
       // groupCopy = tasks
