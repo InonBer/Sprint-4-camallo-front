64989f7d83001839975e26a03b7048eed4183573
Shai <shay4447@gmail.com>
2022-07-28 23:11:00 +0300

    comments component

---
+++ src/cmps/details-activities.vue
@@ -0,0 +1,25 @@
+<template>
+    {{user}}
+</template>
+ <script>
+export default {
+    name: 'details-activities',
+    props: {
+        task: Object
+    },
+    components: {},
+    data() {
+        return {};
+    },
+    created() { },
+    methods: {},
+    computed: {
+        user() {
+            return this.$store.getters.currUser
+        }
+    },
+    unmounted() { },
+};
+</script>
+ <style>
+ </style>
