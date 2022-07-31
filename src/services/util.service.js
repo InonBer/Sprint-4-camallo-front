6f24d7fd21ab914cdc4e5e67f66a5d311a47576e
Shai <shay4447@gmail.com>
2022-07-31 20:34:24 +0300

    responsive txt clr header and cover

--- src/services/util.service.js
+++ src/services/util.service.js
@@ -3,7 +3,8 @@ export const utilService = {
     delay,
     getRandomInt,
     makeId,
-    debounce
+    debounce,
+    isDarkColor
 }
 
 function delay(ms = 1500) {
@@ -25,6 +26,30 @@ function makeId(length = 5) {
         txt += possible.charAt(Math.floor(Math.random() * possible.length))
     }
     return txt
+}
+
+function isDarkColor(c) {
+    if (c[0] !== '#') c = _rgbToHex(c)
+    c = c.substring(1) // strip #
+    const rgb = parseInt(c, 16) // convert rrggbb to decimal
+    const r = (rgb >> 16) & 0xff // extract red
+    const g = (rgb >> 8) & 0xff // extract green
+    const b = (rgb >> 0) & 0xff // extract blue
+    var luma = 0.2126 * r + 0.7152 * g + 0.0722 * b // per ITU-R BT.709
+
+    return luma < 100
+}
+function _componentToHex(c) {
+    var hex = c.toString(16);
+    return hex.length == 1 ? "0" + hex : hex;
+}
+function _rgbToHex(clr) {
+    if (clr[0] === '#') return clr
+    const rgb = clr.split('(')[1].split(')')[0].split(',')
+    const r = +rgb[0].trim()
+    const g = +rgb[1].trim()
+    const b = +rgb[2].trim()
+    return "#" + _componentToHex(r) + _componentToHex(g) + _componentToHex(b);
 }
 
 export function debounce(func, wait = 700) {
